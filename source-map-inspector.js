6996f77440d0e9257932c73abe0e0b868d1f41ca
Fuji, Goro <g.psy.va@gmail.com>
2012-06-20 12:24:36 +0900

    Refactor

--- source-map-inspector.js
+++ source-map-inspector.js
@@ -1,4 +1,24 @@
 window.addEventListener('load', function (e) {
+	"use strict";
+
+	var Lexer = require("jslexer");
+	require("source-map");
+
+	var SOURCE = "source"; // should have data-file="..."
+	var ORIG   = "original";
+	var GEN    = "generated";
+
+	var colorMapping = {
+		space:      "#ccc", // includes comments
+		identifier: "#333",
+		keyword:    "#09d",
+		string:     "#d70",
+		number:     "#f60",
+		regexp:     "#660"
+	};
+
+	var DEBUG = false;
+
 	function element(id) {
 		var item = document.getElementById(id);
 		if(!item) {
@@ -13,8 +33,8 @@ window.addEventListener('load', function (e) {
 
 		// accumulate
 		do {
-		  valueT += elem.offsetTop  || 0;
-		  valueL += elem.offsetLeft || 0;
+		  valueT += elem.offsetTop  | 0;
+		  valueL += elem.offsetLeft | 0;
 		  elem = elem.offsetParent;
 		} while (elem);
 
@@ -40,24 +60,36 @@ window.addEventListener('load', function (e) {
 			var s = tokenObject.token.
 				replace(/&/g, "&amp;").
 				replace(/</g, "&lt;").
-				replace(/>/g, "&gt;")
-				;
+				replace(/>/g, "&gt;");
 
-			return format('<span id="%1%2" class="%3">%4</span>',
-						  prefix, tokenObject.id, tokenObject.type, s);
+			var t = tokenObject.type;
+
+			var style = t in colorMapping ? "color:" + colorMapping[t] : "";
+
+			return format('<span id="%1%2" style="%3;" class="%4">%5</span>',
+						  prefix, tokenObject.id, style, t, s);
 		});
 		return "<code>" + elements.join("") + "</code>";
 	}
 
-	var Lexer = require("jslexer");
-	require("source-map");
+	function initSourcePane(s, p) {
+		p.style.padding = "0px";
+		p.style.margin  = "1px";
+		p.style.width = format("%1px", (s.offsetWidth >> 1) - 4);
+	}
 
-	var gen  = Lexer.tokenize("foo.gen.js", load("foo.gen.js"));
+	var source = element(SOURCE);
+	console.assert(source.dataset.file, "source.dataset.file");
+
+	initSourcePane(source, element(ORIG));
+	initSourcePane(source, element(GEN));
+
+	var gen = Lexer.tokenize(source.dataset.file, load(source.dataset.file));
 
 	var mappingURL;
 
 	if(gen[gen.length-1].type === "space") {
-		m = gen[gen.length-1].token.
+		var m = gen[gen.length-1].token.
 			match(/\@ *sourceMappingURL=([^ \t\r\n]+)/);
 		if(!m) {
 			alert("sourceMappingURL is not found");
@@ -68,29 +100,39 @@ window.addEventListener('load', function (e) {
 	var mapping = JSON.parse(load(mappingURL));
 
 	var orig = Lexer.tokenize(mapping.sources[0], load(mapping.sources[0]));
-	var colorMapping = {
-		identifier: "#333",
-		keyword:    "#09d",
-		string:     "#d70",
-		number:     "#f60",
-		regexp:     "#660"
-	};
-
-	var ORIG = "original";
-	var GEN  = "generated";
 
 	element(ORIG).innerHTML = asHTML(ORIG, orig);
 	element(GEN).innerHTML  = asHTML(GEN,  gen);
 
-
 	var consumer = new sourceMap.SourceMapConsumer(mapping);
 
-	var canvas = element('display');
+	var basePos = {
+		x: source.offsetLeft,
+		y: source.offsetTop
+	};
 
-	canvas.width  = document.documentElement.offsetWidth - 10;
-	canvas.height = document.documentElement.offsetHeight;
+	var cx = (function () {
+		var c = document.createElement('canvas');
 
-	var cx = canvas.getContext("2d");
+		if(DEBUG) {
+			c.style.border = "solid 1px black";
+		}
+
+		c.style.position = "absolute";
+		c.style.margin = "0px";
+		c.style.padding = "0px";
+		c.style.left = source.offsetLeft + "px";
+		c.style.top  = source.offset + "px";
+
+		c.width  = source.offsetWidth;
+		c.height = source.offsetHeight;
+
+		source.appendChild(c);
+
+		return c.getContext("2d");
+	}());
+
+	console.assert(cx, "cx");
 
 	var original = [];
 	orig.forEach(function (tokenObject) {
@@ -100,6 +142,7 @@ window.addEventListener('load', function (e) {
 		original[tokenObject.line][tokenObject.column] = tokenObject;
 	});
 
+	// draw lines
 	for(var i = 0; i < gen.length; ++i) {
 		(function (genToken) {
 			setTimeout(function() {
@@ -120,16 +163,16 @@ window.addEventListener('load', function (e) {
 				// left pain
 				var elem  = element(ORIG + origToken.id);
 				var p = elementOffset(elem);
-				cx.moveTo(p.x, p.y);
+				cx.moveTo(p.x - basePos.x, p.y - basePos.y);
 
 				// to right pain
 				elem = element(GEN + genToken.id);
 				p = elementOffset(elem);
-				cx.lineTo(p.x, p.y);
+				cx.lineTo(p.x - basePos.x, p.y - basePos.y);
 
-				cx.strokeStyle = colorMapping[ genToken.type ];
+				cx.strokeStyle = elem.style.color;
 				cx.stroke();
-			}, i * 3);
+			}, i * 2);
 		}(gen[i]));
 	}
 });
